1cc596742ae14b087bc0f20eda84281611cbbb49
ntquang@it <nguyentriquang23@gmail.com>
2025-11-01 10:33:51 +0700

    fix json response simple

--- controllers/uploadController.js
+++ controllers/uploadController.js
@@ -44,40 +44,40 @@ exports.handleUpload = (req, res) => {
             cmd: "face",
             code: 0,
             sequence_no: body?.sequence_no,
-            cap_time: body?.cap_time,
-            tts: {
-                text: body?.match?.person_name || ""
-            },
-            gateway_ctrl: {
-                device_type: "gpio",
-                device_no: 1,
-                ctrl_mode: "force",
-            },
+            cap_time: body?.cap_time
+            // tts: {
+            //     text: body?.match?.person_name || ""
+            // },
+            // gateway_ctrl: {
+            //     device_type: "gpio",
+            //     device_no: 1,
+            //     ctrl_mode: "force",
+            // },
             
-            text_displays: [
-                {
-                    position: { 
-                        x: 0, 
-                        y: 450 
-                    },
-                    alive_time: 5000,
-                    font_size: 120,
-                    font_spacing: 1,
-                    font_color: "0xff00ff00",
-                    text: body?.match?.person_name || ""
-                },
-                {
-                    position: { 
-                        x: 0, 
-                        y: 300 
-                    },
-                    alive_time: 5000,
-                    font_size: 50,
-                    font_spacing: 1,
-                    font_color: "0xffff0000",
-                    text: body?.match?.person_name || ""
-                }
-            ]
+            // text_displays: [
+            //     {
+            //         position: { 
+            //             x: 0, 
+            //             y: 450 
+            //         },
+            //         alive_time: 5000,
+            //         font_size: 120,
+            //         font_spacing: 1,
+            //         font_color: "0xff00ff00",
+            //         text: body?.match?.person_name || ""
+            //     },
+            //     {
+            //         position: { 
+            //             x: 0, 
+            //             y: 300 
+            //         },
+            //         alive_time: 5000,
+            //         font_size: 50,
+            //         font_spacing: 1,
+            //         font_color: "0xffff0000",
+            //         text: body?.match?.person_name || ""
+            //     }
+            // ]
         };
 
         return res.status(200).json(ackResponse);
